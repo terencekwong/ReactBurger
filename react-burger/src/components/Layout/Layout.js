f298ec7389f7e72bbf9a9843949a9bab4d911c08
Terence <terence.kwong@health.nsw.gov.au>
2019-11-27 16:54:44 +1100

    finsihed Section 8

--- react-burger/src/components/Layout/Layout.js
+++ react-burger/src/components/Layout/Layout.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxs from '../../hoc/Auxs';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxs>
-        <Toolbar />
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((preState) => {
+            return {showSideDrawer: !preState.showSideDrawer};
+        });
+    }
+
+    render () {
+        return <Auxs>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer 
+            open={this.state.showSideDrawer} 
+            closed={this.sideDrawerClosedHandler}/>
         <main className={classes.Content}>
-            { props.children }
+            { this.props.children }
         </main>
     </Auxs>
-);
+    }
+};
 
-export default layout;
+export default Layout;
