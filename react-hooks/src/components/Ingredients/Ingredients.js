32041a00d0ee42b498d24577711ab63cf3d10627
Terence <terence.kwong@health.nsw.gov.au>
2019-12-11 16:36:35 +1100

    Finsihed Hooks 466

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]); 
+
+  useEffect(() => {
+    fetch('https://react-test-tk.firebaseio.com/ingedients.json')
+    .then(response => {
+      return response.json();
+    })
+    .then(responseData => {
+      const loadIngredients = [];
+      for(const key in responseData) {
+        loadIngredients.push({ 
+          id: key, 
+          title: responseData[key].title, 
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadIngredients);
+    })
+  }, []);
+
+  const addIngredientHandler = ingredient => {
+    fetch('https://react-test-tk.firebaseio.com/ingedients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: {'Content-Type': 'application/json'}
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => 
+        [
+          ...prevIngredients, 
+          { id: responseData.name, ...ingredient }
+        ]);
+    });
+  };
+
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients => 
+      prevIngredients.filter(x => x.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
